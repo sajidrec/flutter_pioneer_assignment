1c1a497fad506b0ba6b121b676cdd4437e3c7047
sajid <sajidrec@gmail.com>
2025-12-13 17:58:45 +0600

    - add initial commit

---
+++ android/app/src/main/kotlin/com/pioneer/assignment/sajidrec/flutter_pioneer_assignment/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.pioneer.assignment.sajidrec.flutter_pioneer_assignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,14 @@
+import 'package:get/get.dart';
+import '../modules/home_page/presentation/bindings/home_binding.dart';
+import '../modules/home_page/presentation/views/home_page.dart';
+import 'app_routes.dart';
+
+class AppPages {
+  static final List<GetPage> pages = [
+    GetPage(
+      name: AppRoutes.homeRoute,
+      page: () => HomePage(),
+      binding: HomeBinding(),
+    ),
+  ];
+}
