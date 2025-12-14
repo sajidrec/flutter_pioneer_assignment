0608a0982acf0857924388bded546a9e3d407257
sajid <sajidrec@gmail.com>
2025-12-14 11:42:16 +0600

    - add error handling on splash page network error

--- lib/app/core/constants/app_colors.dart
+++ lib/app/core/constants/app_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class AppColors {
   static Color red = Color(0xffff0000);
   static Color white = Color(0xffffffff);
+  static Color green = Color(0xff00ff00);
 }
--- lib/app/modules/splash_page/presentation/controllers/splash_page_controller.dart
+++ lib/app/modules/splash_page/presentation/controllers/splash_page_controller.dart
@@ -7,19 +7,29 @@ import 'package:flutter_pioneer_assignment/app/core/errors/app_exceptions.dart';
 import 'package:flutter_pioneer_assignment/app/core/network/api_endpoints.dart';
 import 'package:flutter_pioneer_assignment/app/core/network/dio_client.dart';
 import 'package:flutter_pioneer_assignment/app/modules/splash_page/data/models/repo_list_model.dart';
+import 'package:flutter_pioneer_assignment/app/routes/app_routes.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashPageController extends GetxController {
   int loaderDots = 1;
+  int waitingSteps = 0;
 
   Future<void> startDotLoading() async {
     Timer.periodic(Duration(milliseconds: 500), (timer) {
+      waitingSteps++;
       loaderDots++;
       loaderDots = loaderDots % 7;
       loaderDots == 0 ? loaderDots = 1 : loaderDots;
       update();
     });
+  }
+
+  Future<void> _moveToNextPage() async {
+    if (waitingSteps < 5) {
+      await Future.delayed(Duration(seconds: 2));
+    }
+    Get.offAllNamed(AppRoutes.homeRoute);
   }
 
   Future<void> _fetchDataFromInternet() async {
@@ -37,6 +47,10 @@ class SplashPageController extends GetxController {
         AppKeys.dataStoreKey,
         jsonEncode(repoListModel.toJson()),
       );
+
+      // print("sajid testing ${repoListModel.items}");
+
+      _moveToNextPage();
     } on AppException catch (e) {
       Get.snackbar(
         "Error occured",
@@ -44,6 +58,29 @@ class SplashPageController extends GetxController {
         backgroundColor: AppColors.red,
         colorText: AppColors.white,
       );
+
+      SharedPreferences sharedPreferences =
+          await SharedPreferences.getInstance();
+
+      String data = sharedPreferences.getString(AppKeys.dataStoreKey) ?? "";
+
+      if (data.isEmpty) {
+        Get.snackbar(
+          "Something went wrong",
+          "Most probably network error",
+          backgroundColor: AppColors.red,
+          colorText: AppColors.white,
+        );
+      } else {
+        Get.snackbar(
+          "We will show old fetched data",
+          "",
+          backgroundColor: AppColors.green,
+          colorText: AppColors.white,
+        );
+
+        _moveToNextPage();
+      }
     } catch (e) {
       Get.snackbar(
         "Error occured",
